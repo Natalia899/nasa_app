6aad9c660c44b70d19131370cb76322e88a6a4cf
Natalia <nataliostapiuk@gmail.com>
2020-12-31 01:56:43 +0200

    like button added

--- src/components/container/MediaCard.js
+++ src/components/container/MediaCard.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-
+import FavoriteIcon from '@material-ui/icons/Favorite';
 export default function MediaCard(props) {
-
 
     const save = async() => {
         let newLike = props.picture
@@ -11,17 +10,17 @@ export default function MediaCard(props) {
     }
 
    const disLike = async() => {
-       console.log(props.picture._id);
-       console.log(props);
        props.disLike(props.picture._id)
    }
     return (
         <div>
             <p>{props.picture.title}</p>
-            <img src={props.picture.image} alt='' />
+            {props.picture.media_type === 'video' ? <iframe src={props.picture.image} /> :  <img src={props.picture.image} alt='' />}
+            {/* <img src={props.picture.image} alt='' /> */}
             <div>{props.picture.explanation}</div>
             {props.picture.likeButton === 'liked' ? <span onClick={save}>like</span> : 
             props.picture.likeButton === 'disLike' ? <span onClick ={disLike}>unlike</span> : null}
         </div>
     )
 }
+
